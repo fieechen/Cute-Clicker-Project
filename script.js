1961cdcf5b096e42f1cc71fea64fcca4323f97c2
Sofie <sofie.kubiak@gmx.de>
2025-11-25 13:57:14 +0100

    JS: Implemented Boost feature, also fixed wrong Button ID´s

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let clickPower = 1; // normale Klickstärke
 let upgrade1Unlocked = false;
 let upgrade2Unlocked = false;
 let upgrade3Unlocked = false;
+let boostUnlocked = false;
 
 const counter = document.getElementById("counter");
 const clickBtn = document.getElementById("clickImg");
-const upgrade1 = document.getElementById("upgrade1");
-const upgrade2 = document.getElementById("upgrade2");
-const upgrade3 = document.getElementById("upgrade3");
+const upgrade1 = document.getElementById("button1");
+const upgrade2 = document.getElementById("button2");
+const upgrade3 = document.getElementById("button3");
+const boost = document.getElementById("button4");
 
 
 // Klick-Logik für Hauptbutton
@@ -34,13 +36,19 @@ clickBtn.addEventListener("click", () => {
         upgrade3.disabled = false;
         upgrade3Unlocked = true;
     }
+
+    // Boost freischalten
+    if (!boostUnlocked && count >= 300) {
+        boost.disabled = false;
+        boostUnlocked = true;
+    }
 });
 
 // Klick-Logik für Upgrade 1
 upgrade1.addEventListener("click", () => {
     if (count >= 50) {
         count -= 50; // Upgrade kostet 50 Beans
-        clickPower = 2; // jetzt +2 pro Klick
+        clickPower = +2; // jetzt +2 pro Klick
         counter.textContent = count;
         upgrade1.disabled = true; // optional: nur einmal kaufbar
     }
@@ -49,7 +57,7 @@ upgrade1.addEventListener("click", () => {
 upgrade2.addEventListener("click", () => {
     if (count >= 100) {
         count -= 100; // Upgrade kostet 50 Beans
-        clickPower = 4; // jetzt +2 pro Klick
+        clickPower = +4; // jetzt +2 pro Klick
         counter.textContent = count;
         upgrade2.disabled = true; // optional: nur einmal kaufbar
     }
@@ -58,8 +66,16 @@ upgrade2.addEventListener("click", () => {
 upgrade3.addEventListener("click", () => {
     if (count >= 150) {
         count -= 150; // Upgrade kostet 50 Beans
-        clickPower = 8; // jetzt +2 pro Klick
+        clickPower =+8; // jetzt +2 pro Klick
         counter.textContent = count;
         upgrade3.disabled = true; // optional: nur einmal kaufbar
+    }
+});
+boost.addEventListener("click", () => {
+    if (count >= 300) {
+        count -= 300; // Upgrade kostet 50 Beans
+        clickPower = *2; // 
+        counter.textContent = count;
+        boost.disabled = true; // optional: nur einmal kaufbar
     }
 });
